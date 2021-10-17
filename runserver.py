25e77a8df36da22537cd669e5b13b4ca51baece3
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-10-17 20:50:18 +0100

    j

--- runserver.py
+++ runserver.py
@@ -2,16 +2,23 @@
 This script runs the RC application using a development server.
 """
 
-from os import environ
-from App import app
+from os import environ, truncate
+from re import A
+from App import app,application
 
+
+
+    
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
 
 if __debug__:
-    
-    if __name__ == '__main__':
-        HOST = environ.get('SERVER_HOST', 'localhost')
-        try:
-            PORT = int(environ.get('SERVER_PORT', '5555'))
-        except ValueError:
-            PORT = 5555
+        application.debug = True
         app.run(HOST, PORT)
+else:
+        application.debug = False
+        app.run()
