694bb298714d0807314ccfa2453fff5bb8b2e3dc
tomasvicente20002 <56982030+tomasvicente20002@users.noreply.github.com>
2021-10-17 20:54:47 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -7,18 +7,17 @@ from re import A
 from App import app,application
 
 
-
-    
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
+    if __debug__:     
+        HOST = environ.get('SERVER_HOST', 'localhost')
+        try:
+            PORT = int(environ.get('SERVER_PORT', '5555'))
+        except ValueError:
+            PORT = 5555
 
-if __debug__:
+
         application.debug = True
         app.run(HOST, PORT)
-else:
+    else:
         application.debug = False
         app.run()
